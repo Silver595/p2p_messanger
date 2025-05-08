5f4a3518c215d41076adb75c4162bcb328122e24
Silver595 <akashpurjalkar66@gmail.com>
2025-05-08 16:37:49 +0000

    Tickets implementd

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::fmt;
+use std::str::FromStr;
+
 use anyhow::Result;
 use futures_lite::StreamExt;
 use iroh::protocol::Router;
-use iroh::{Endpoint, SecretKey};
+use iroh::{Endpoint,NodeAddr, SecretKey};
 use iroh_gossip::net::{Event,Gossip, GossipEvent, GossipReceiver};
 use iroh_gossip::proto::TopicId;
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -48,4 +52,35 @@ async fn main() -> Result<()> {
         
         }
         Ok(())
+    }
+
+    #[derive(Debug,Serialize,Deserialize)]
+    struct Ticket {
+        topic: TopicId,
+        peers:Vec<NodeAddr>,
+    }
+
+    impl Ticket {
+        fn from_bytes(bytes: &[u8]) -> Result<Self> {
+            serde_json::from_slice(bytes).map_err(Into::into)
+        }
+        pub fn to_bytes(&self) -> Vec<u8> {
+            serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+        }
+    }
+
+    impl fmt::Display for Ticket {
+        fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+            let mut text = data_encoding::BASE32_NOPAD.encode(&self.to_bytes()[..]);
+            text.make_ascii_lowercase();
+            write!(f,"{}",text)
+        }
+    }
+
+    impl FromStr for Ticket {
+        type Err = anyhow::Error;
+        fn from_str(s: &str) -> Result<Self,Self::Err> {
+            let bytes = data_encoding::BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
+            Self::from_bytes(&bytes)
+        }
     }
